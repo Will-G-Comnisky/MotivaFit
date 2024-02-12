826595fb9405d0de9ff9c193ec95efc22877128a
William <william.gispiela@gmail.com>
2024-02-11 21:40:42 -0300

    15.001:feat: initial commit, login logic made in routes.ts with db mock up

--- backend/src/server/Server.ts
+++ backend/src/server/Server.ts
@@ -3,15 +3,15 @@ import fs from 'fs';
 import app from '../app';
 
 
+
 const server = spdy.createServer({
     key: fs.readFileSync("key.pem", "utf-8"),
     cert: fs.readFileSync("cert.pem", "utf-8")
 }, app);
 
 
-const PORT = process.env.PORT || 3001
-server.listen(PORT, ()=>{
-    console.log(`Server Rodando em https://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, ()=> console.log(`Server Rodando em https://localhost:${PORT}`));
 
 
