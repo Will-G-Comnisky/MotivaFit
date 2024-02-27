8c705f626cf03d23d415934d5169e00d5527967f
William <william.gispiela@gmail.com>
2024-02-27 13:45:53 -0300

    removido authenticate, middleware de permissão, das rotas

--- backend/src/routes/aluno.routes.ts
+++ backend/src/routes/aluno.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { AlunoController } from "../app/controllers/AlunoController";
 import { verifyToken } from "../middlewares/auth";
-import { authorize } from "../middlewares/permission";
+//import { authorize } from "../middlewares/permission";
 
 const alunoRoutes = (router: Router) => {
   router.post("/aluno", AlunoController.createAluno);
   router.get("/aluno", verifyToken, AlunoController.getAllAlunos);
-  router.get("/aluno/:id", verifyToken, authorize, AlunoController.getUserById);
-  router.put("/aluno/:id", verifyToken, authorize, AlunoController.updateAluno);
-  router.delete("/aluno/:id", verifyToken, authorize, AlunoController.deleteAluno);
-  router.get('/admin/:adminId/alunos', verifyToken, authorize, AlunoController.getAllAlunosByAdminId);
+  router.get("/aluno/:id", verifyToken, AlunoController.getUserById);
+  router.put("/aluno/:id", verifyToken, AlunoController.updateAluno);
+  router.delete("/aluno/:id", verifyToken, AlunoController.deleteAluno);
+  router.get('/admin/:adminId/alunos', verifyToken, AlunoController.getAllAlunosByAdminId);
   //router.get('/aluno/me', verifyToken, AlunoController.getOwnUserData); // Rota para um aluno obter seus próprios dados
   //router.put('/aluno/me', verifyToken, AlunoController.updateOwnUserData); // Rota para um aluno atualizar seus próprios dados
 };
--- backend/src/routes/avaliacao.routes.ts
+++ backend/src/routes/avaliacao.routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { AvaliacoesController } from "../app/controllers/AvaliacoesController";
 import { verifyToken } from "../middlewares/auth";
-import { authorize } from "../middlewares/permission";
+//import { authorize } from "../middlewares/permission";
 
 const avaliacaoRoutes = (router: Router) => {
   router.post("/avaliacao", verifyToken, AvaliacoesController.createAvaliacao);
   router.get("/avaliacao", verifyToken, AvaliacoesController.getAllAvaliacoes);
-  router.get("/avaliacao/:id", verifyToken, authorize, AvaliacoesController.getAvaliacoById);
-  router.put("/avaliacao/:id", verifyToken, authorize, AvaliacoesController.updateAvaliacao);
-  router.delete("/avaliacao/:id", verifyToken, authorize, AvaliacoesController.deleteAvaliacao);
+  router.get("/avaliacao/:id", verifyToken, AvaliacoesController.getAvaliacoById);
+  router.put("/avaliacao/:id", verifyToken, AvaliacoesController.updateAvaliacao);
+  router.delete("/avaliacao/:id", verifyToken, AvaliacoesController.deleteAvaliacao);
 };
 
 export default avaliacaoRoutes;
