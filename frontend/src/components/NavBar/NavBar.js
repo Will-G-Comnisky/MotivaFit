6f4f8d3b0c5da0b1c391c68aa82bf8a461328793
marcus-piccoli <marcus.piccoli@gmail.com>
2024-02-28 12:34:02 -0300

    frontEndMarcus

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,45 +1,63 @@
 import { useNavigate } from "react-router-dom";
-import styles from "./NavBar.module.css"
+import styles from "./NavBar.module.css";
 
-function NavBar(props){
-    const navigate = useNavigate();
+function NavBar(props) {
+  const navigate = useNavigate();
 
-    const handleLogout = () => {
-        // Aqui você pode adicionar a lógica de logout
-        // Primeiro, removemos o usuário do localStorage
-        localStorage.removeItem("user");
+  const handleLogout = () => {
+    // Aqui você pode adicionar a lógica de logout
+    // Primeiro, removemos a sessão do localStorage
+    localStorage.removeItem("session");
 
-        // Depois de fazer logout, redirecionamos o usuário
-        navigate("/");
+    // Depois de fazer logout, redirecionamos o usuário
+    navigate("/");
+  };
+
+  const handleHome = () => {
+    // Obtenha o usuário do localStorage
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    // Verifique o tipo de usuário e navegue para a página inicial correspondente
+    if (user && user.type === "aluno") {
+      navigate("/AlunoMainPage");
+    } else if (user && user.type === "professor") {
+      navigate("/PersonalTrainerMainPage");
+    } else {
+      // Se não houver usuário no localStorage ou o tipo de usuário for desconhecido, navegue para a página inicial padrão
+      navigate("/");
     }
+  };
 
-    const handleHome = () => {
-        // Depois de fazer logout, redirecionamos o usuário
-        navigate("/PersonalTrainerMainPage");
-    }
+  return (
+    <div id={styles.idNav}>
+      <div id={styles.idNavImgBackground}>
+        {/* ----- Imagem ----- */}
+        <img
+          src={props.logo}
+          id={styles.idNavImg}
+          alt=""
+          onClick={handleHome}
+        />
+      </div>
+      <div id={styles.idLogoName} onClick={handleHome}>
+        Motiva Fit
+      </div>
 
-    return(
-        
-        <div id={styles.idNav}>
+      <div id={styles.idFiller}></div>
 
-            <div id={styles.idNavImgBackground}>
-                {/* ----- Imagem ----- */}
-                <img src={props.logo} id={styles.idNavImg} alt="" onClick={handleHome}/>
-            </div>
-            <div id={styles.idLogoName} onClick={handleHome}>
-                Motiva Fit
-            </div>
+      <div>
+        <button className={styles.clNavBotao} onClick={handleHome}>
+          Início
+        </button>
+      </div>
 
-            <div id={styles.idFiller}></div>
-
-            <div><button className={styles.clNavBotao} onClick={handleHome}>Início</button></div>
-
-            <div className={styles.clNavBotaoBackground}>
-                <button className={styles.clNavBotao} onClick={handleLogout}>Logout</button>
-            </div>
-            
-        </div>
-    )
+      <div className={styles.clNavBotaoBackground}>
+        <button className={styles.clNavBotao} onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
